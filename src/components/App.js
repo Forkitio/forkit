98f493d75e68412592d985d9fb21bbfb5b3e2059
kylie gao <kylie.y.gao@gmail.com>
2018-11-23 10:29:10 -0500

    added material UI to the navbar, and dashboard and cookbook files

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React, { Component, Fragment } from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Nav from './Nav'
+import Cookbook from './Cookbook'
+import Dashboard from './Dashboard'
 import LoginPage from './LoginPage'
 import { exchangeTokenForAuth } from '../store/authStore'
 import queryString from 'query-string'
 
+
 class App extends Component {
+
   componentDidMount() {
     const query = queryString.parse(this.props.location.search);
     if(query.token) {
@@ -15,12 +19,18 @@ class App extends Component {
     }
     this.props.tokenCheck();
   }
+
   render(){
+    //KG note: once I can figure out how to get auth working, cookbook route should be '/user/:userId/cookbook', dashboard route should be '/user/:userId/dashboard'
+
+
     return (
       <Router>
         <Fragment>
           <Nav />
           <Route exact path='/login' component={LoginPage} />
+          <Route path = '/user/cookbook' render = {() => <Cookbook /> }/>
+          <Route path = '/user/dashboard' render = {() => <Dashboard />} />
         </Fragment>
       </Router>
     )
---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component {
+    render () {
+        return (
+            <Fragment>
+                <h1>Dashboard</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default Dashboard
